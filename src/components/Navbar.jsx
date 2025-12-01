142c299a6fac8e1812011815c88faa874757a5ba
spothini <pothinis@gmail.com>
2025-11-30 22:24:55 -0500

    fourth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
                 onClick={handleClick}
                 className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
               >
-                Create  one account
+                Create an account
               </a>
             </div>
           </div>
